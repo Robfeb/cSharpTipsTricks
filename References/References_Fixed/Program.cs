cb9daddb795f5b21b98d42ccd3aafe7b5853833e
Roberto Febrer <febrer@gmail.com>
2023-02-04 19:26:22 +0000

    add References

---
+++ References/References_Fixed/Program.cs
@@ -0,0 +1,12 @@
+﻿var ninjas = new[] { "Itachi", "Kisame", "Naruto" };
+for (var i = 0; i < 3; i++)
+{
+    AddGreeting(ref ninjas[i]);
+}
+foreach (var ninja in ninjas)
+{
+    Console.WriteLine(ninja);
+}
+
+static void AddGreeting(ref string ninja)
+    => ninja = "Greetings " + ninja;
---
+++ References/References_Wrong/Program.cs
@@ -0,0 +1,13 @@
+﻿var ninjas = new[] { "Itachi", "Kisame", "Naruto" };
+foreach (var ninja in ninjas)
+{
+    AddGreeting(ninja);
+}
+foreach (var ninja in ninjas)
+{
+    Console.WriteLine(ninja);
+}
+
+
+static void AddGreeting(string ninja)
+    => ninja = "Greetings " + ninja;
