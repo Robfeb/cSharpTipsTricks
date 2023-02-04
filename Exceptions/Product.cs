5ae2eefc35d8597e7bb42b0028dbcfc133b5cf29
Roberto Febrer <febrer@gmail.com>
2023-02-04 22:11:34 +0000

    add Exceptions

---
+++ Exceptions/Product.cs
@@ -0,0 +1,8 @@
+namespace Exceptions
+{
+    class Product
+    {
+        public string? Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ Exceptions/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Enter product name:");
+                    var productName = Console.ReadLine();
+                    Console.WriteLine("Enter desired price:");
+                    var price = decimal.Parse(Console.ReadLine());
+                    var prod = new Product() { Name = productName, Price = price };
+                    DoShopping(prod);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"An Error Ocurred {e.Message}");
+                    throw;
+                }
+            }
+        }
+
+        static void DoShopping(Product p)
+        {
+            CalculateTax(p);
+        }
+
+        static void CalculateTax(Product p)
+        {
+            try
+            {
+                var previousPrice = p.Price;
+                p.Price += 1M;
+                var taxIncrease = (p.Price - previousPrice) / previousPrice;
+                Console.WriteLine($"Price increase {taxIncrease.ToString("P")}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
